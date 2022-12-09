4b1e6cd2a683869814b4e3a868fe00a8ce32ead9
Muhammad Tatma <muhammadtatma04@gmail.com>
2022-12-10 00:28:27 +0700

    add firebase and signup functionality

--- app/src/main/java/com/example/ez_math/LoginActivity.kt
+++ app/src/main/java/com/example/ez_math/LoginActivity.kt
@@ -1,11 +1,19 @@
 package com.example.ez_math
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val signuplink = findViewById<TextView>(R.id.signuplink)
+        signuplink.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
+
     }
 }
