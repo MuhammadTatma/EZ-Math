747e6773f615f20c1ecbf600aae368c0f1e556cc
intannabila09 <intan09azmi@gmail.com>
2022-12-13 13:45:59 +0700

    halaman latihan

---
+++ app/src/main/java/com/example/ez_math/HasilLatihan.kt
@@ -0,0 +1,27 @@
+package com.example.ez_math
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import com.example.ez_math.Fragments.FragmentPencapaian
+
+class HasilLatihan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hasil_latihan)
+    }
+
+    fun fKePencapaian(view: View){
+//        val etPesan = findViewById<EditText>(R.id.etPesan)
+//        val isiPesan = etPesan.text.toString()
+
+        val intentKePencapaian = Intent(this, FragmentPencapaian::class.java)
+//        intentKeLinear.apply {
+//            putExtra(KUNCI, isiPesan)
+//        }
+        startActivity(intentKePencapaian)
+    }
+
+}
